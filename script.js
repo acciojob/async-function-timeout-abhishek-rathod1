a306c2c17cd21dcdd6295656889b6e7ccf7fefb7
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-05-01 18:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+// button.addEventListener("click", fxn(msg,dly));
+
+async function fxn(){
+	const msg = document.getElementById("text").value;
+	const dly = document.getElementById("delay").value;
+	const button = document.getElementById("btn");
+	console.log("message: ", msg);
+	console.log("Delay: ", dly);
+	await setTimeout(()=>{
+		console.log("message: ", msg);
+		console.log("Delay: ", dly);
+		document.getElementById("output").innerHTML = `<p>${msg}</p>`;
+		
+	},dly*1000)
+}
+
+//dd dd
